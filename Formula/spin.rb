5be194ef6e27914cb8ed0c0533ca29ed457a85f0
Les Hazlewood <les@hazlewood.com>
2017-05-01 09:10:42 -0700

    Update spin.rb

--- Formula/spin.rb
+++ Formula/spin.rb
@@ -3,7 +3,7 @@ class Spin < Formula
   desc 'A command-line tool for service and resource orchestration.'
   homepage 'https://github.com/lhazlewood/spin'
   url 'http://repo1.maven.org/maven2/com/leshazlewood/spin/spin/0.2.0/spin-0.2.0.zip'
-  sha256 '56ca4942d727410b9c95b84ba877fb8d966d88a29d9bac81d0c10ea434695516'
+  sha256 '918b24dab989ffda6c3c867aa227fced8f0d6c0da0c838ba61ae7f3e258175b5'
   version '0.2.0'
 
   bottle :unneeded
